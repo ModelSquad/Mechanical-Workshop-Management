6cf85497f4f148c403722a2668476b9ecfb8f523
Sevi7 <adrisevilla98@gmail.com>
2018-11-27 17:34:23 +0100

    installed connect-flash and fixed bugs

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -6,18 +6,28 @@ module.exports = {
   postLogin : function(req, res, next){
     var usuario = req.body.user;
     var password = req.body.pass;
+    var info = req.body.txt1;
 
     var connection = mysql.createConnection(connectionInfo);
+    function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
     connection.query('SELECT * FROM `dbo.tUsuario` WHERE `nombre` = ?', usuario, function(err, rows, fields) {
       connection.end();
-
       if(!err) {
         if(rows.length > 0) {
           var userDB = rows[0];
           if(userDB.password === password) {
             return res.redirect('/home');
           } else {
-            throw err;
+            info='Usuario o constraseña incorrecto';
+            //podemos hacer que se recarge la pagina con el info cambiado.
+            wait(10000);
+            return res.redirect('/');
           }
 
         } else {
@@ -26,7 +36,6 @@ module.exports = {
       } else {
        throw err;
       }
-      throw err;
     });
 
   }
