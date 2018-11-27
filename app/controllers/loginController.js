7d8810635d37a283ac8eb5c4782d14dea6d5255a
jaysusm <jmartinruiz@uma.es>
2018-11-27 17:55:34 +0100

    Fix Sevi-made bugs

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -9,13 +9,6 @@ module.exports = {
     var info = req.body.txt1;
 
     var connection = mysql.createConnection(connectionInfo);
-    function wait(ms){
-   var start = new Date().getTime();
-   var end = start;
-   while(end < start + ms) {
-     end = new Date().getTime();
-  }
-}
     connection.query('SELECT * FROM `dbo.tUsuario` WHERE `nombre` = ?', usuario, function(err, rows, fields) {
       connection.end();
       if(!err) {
@@ -26,12 +19,13 @@ module.exports = {
           } else {
             info='Usuario o constraseña incorrecto';
             //podemos hacer que se recarge la pagina con el info cambiado.
-            wait(10000);
             return res.redirect('/');
           }
 
         } else {
-          throw err;
+          info='Usuario o constraseña incorrecto';
+          //podemos hacer que se recarge la pagina con el info cambiado.
+          return res.redirect('/');
         }
       } else {
        throw err;
