2fbcf496deedd438c9da882616f02968b1a0d076
jaysusm <jmartinruiz@uma.es>
2018-12-04 00:36:32 +0100

    Fix bugs, add alert when fields are empty or not type selected

--- app/controllers/actionButtonsController.js
+++ app/controllers/actionButtonsController.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
 var connectionInfo = require('.././database/connection.js');
+var dialog = require('dialog');
 
 module.exports = {
   addElement : function(req, res, next){
     var name = req.body.nombre;
     var manufacturer = req.body.fabricante;
     var action = req.body.boton; //get the action: add, update or delete
+    var errorOcurred = false;
+
+    if(name === '' || manufacturer === '') {
+      dialog.err('No dejes ningún campo vacío');
+      res.redirect('back');
+      return;
+    }
 
     console.log(action);
 
@@ -14,8 +22,18 @@ module.exports = {
       var id_tipo;
       var nombreTipo = req.get('referer').split('/')[3];
       connection.query('SELECT * FROM `dbo.tTipoPieza` WHERE NOMBRE=\'' + nombreTipo + '\'', function(err, result, field) {
+        if(result[0] === undefined) {
+          dialog.err('Selecciona un tipo antes');
+          errorOcurred = true;
+          return;
+        }
         id_tipo = result[0]['ID_TIPO'];
       });
+
+      if(errorOcurred) {
+        res.redirect('back');
+        return;
+      }
 
       connection.query('SELECT MAX(ID) AS id FROM `dbo.tPiezas`',function(err, result, fields) {
       if(!err){
