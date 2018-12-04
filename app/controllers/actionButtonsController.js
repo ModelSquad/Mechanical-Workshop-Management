6b4886050083709b6da98b6d93459b75d02d2064
Sevi7 <adrisevilla98@gmail.com>
2018-12-04 13:21:24 +0100

    empezado la accion de modificar

--- app/controllers/actionButtonsController.js
+++ app/controllers/actionButtonsController.js
@@ -64,13 +64,56 @@ module.exports = {
         res.redirect('back');
         }
         else if(action==="update"){
+          var connection = mysql.createConnection(connectionInfo);
+          var id_tipo;
+          var nombreTipo = req.get('referer').split('/')[3];
+          connection.query('SELECT * FROM `dbo.tTipoPieza` WHERE NOMBRE=\'' + nombreTipo + '\'', function(err, result, field) {
+            if(result[0] === undefined) {
+              dialog.err('Selecciona un tipo antes');
+              errorOcurred = true;
+              return;
+            }
+            id_tipo = result[0]['ID_TIPO'];
+          });
 
-        }
+          if(errorOcurred) {
+            res.redirect('back');
+            return;
+          }
+
+          connection.query('SELECT MAX(ID) AS id FROM `dbo.tPiezas`',function(err, result, fields) {
+          if(!err){
+            id = result[0].id;
+            var values = {};
+            values.ID = id+1;
+            values.NOMBRE = name;
+            values.FABRICANTE = manufacturer;
+            values.ID_TIPO = id_tipo;
+            console.log(values);
+
+              //var values = {id: id+1,name : name, manufacturer : manufacturer, tipo : name};
+            connection.query('INSERT INTO `dbo.tPiezas` SET ?',[values],function(err, result, fields) {
+                if (!err) {
+                        console.log('Successfully added information.');
+                        console.log(err);
+                    } else {
+                        console.log(result);
+                        console.log(err);
+                        console.log('Was not able to add information to database.');
+                    }
+                connection.end();
+              });
+
+              }
+            });
+
+            res.redirect('back');
+
+            }
         else if(action==="delete"){
 
         }
 
   }
-
 
   }
