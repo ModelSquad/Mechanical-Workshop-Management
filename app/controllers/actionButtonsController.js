2ff6d9639050a9bd0edb90945cc3c83c273abea7
Sevi7 <adrisevilla98@gmail.com>
2018-12-04 13:56:40 +0100

    modificado accion modificar, falta coger datos clickables

--- app/controllers/actionButtonsController.js
+++ app/controllers/actionButtonsController.js
@@ -79,36 +79,28 @@ module.exports = {
             res.redirect('back');
             return;
           }
-
-          connection.query('SELECT MAX(ID) AS id FROM `dbo.tPiezas`',function(err, result, fields) {
-          if(!err){
-            id = result[0].id;
-            var values = {};
-            values.ID = id+1;
-            values.NOMBRE = name;
-            values.FABRICANTE = manufacturer;
-            values.ID_TIPO = id_tipo;
-            console.log(values);
+          var values = {};
+          //values.ID = coger id de la tabla;
+          values.NOMBRE = name;
+          values.FABRICANTE = manufacturer;
+          values.ID_TIPO = id_tipo;
+          console.log(values);
 
               //var values = {id: id+1,name : name, manufacturer : manufacturer, tipo : name};
-            connection.query('INSERT INTO `dbo.tPiezas` SET ?',[values],function(err, result, fields) {
-                if (!err) {
-                        console.log('Successfully added information.');
-                        console.log(err);
-                    } else {
-                        console.log(result);
-                        console.log(err);
-                        console.log('Was not able to add information to database.');
-                    }
-                connection.end();
-              });
+          connection.query('UPDATE `dbo.tPiezas` SET ?',[values],function(err, result, fields) {
+               if (!err) {
+                       console.log('Successfully updated information.');
+                       console.log(err);
+               } else {
+                      console.log(result);
+                      console.log(err);
+                      console.log('Was not able to update information to database.');
+               }
+               connection.end();
+          });
+        res.redirect('back');
 
-              }
-            });
-
-            res.redirect('back');
-
-            }
+        }
         else if(action==="delete"){
 
         }
--- app/database/connection.js
+++ app/database/connection.js
@@ -1,13 +1,8 @@
 var connectionInfo = {
   host  : 'localhost',
   port  : 3306,
-<<<<<<< HEAD
   user  : 'user',
   password : 'password',
-=======
-  user  : 'root',
-  password : 'root',
->>>>>>> 904729b1c154a3625d7d081a6fdbccde0baebab6
   database : 'TrabajoGI1819'
 };
 
